120ff3cf7eb347e4752fe41537b00e5b892d7d31
unknown <jason.rettinger@gmail.com>
2022-09-19 21:01:46 -0400

    added getDepartments and getRoles functions to index.js

--- server.js
+++ server.js
@@ -3,3 +3,22 @@ const dbconfig = require("./dbconfig");
 const connection = mysql.createConnection(dbconfig);
 
 const inquirer = require("inquirer");
+
+const getDepartments = function () {
+    connection.query("SELECT * FROM `department`", function (err, results) {
+      if (err) throw new Error(err);
+      console.table(results);
+      main();
+    });
+  };
+
+  const getRoles = function () {
+    connection.query(
+      "SELECT role.id, title, salary, department_id, department.name FROM `role` INNER JOIN department ON (role.department_id = department.id) ORDER BY role.id ASC",
+      function (err, results) {
+        if (err) throw new Error(err);
+        console.table(results);
+        main();
+      }
+    );
+  };
