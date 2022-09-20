53319c7ae86184a4397ee8e273082d444d4f1bf1
unknown <jason.rettinger@gmail.com>
2022-09-19 21:34:22 -0400

    added addEmployee and then to server.js

---
+++ dbconfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    host: "localhost",
+    // mySQL user name
+    user: 'root',
+    // database name
+    database: 'mycompany_db',
+    // mySQL password
+    password: '',
+  };
+  
--- server.js
+++ server.js
@@ -33,4 +33,65 @@ const getDepartments = function () {
       }
     );
   };
-  
+
+  const addDepartment = function () {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "departmentName",
+          message: "Enter the Department name",
+        },
+      ])
+      .then((answer) => {
+        let name = answer.departmentName;
+        connection.query(
+          "INSERT INTO `department` (name) VALUES (?)",
+          [name],
+          function (err, results) {
+            if (err) throw new Error(err);
+            console.log("Department Added");
+            main();
+          }
+        );
+      });
+  };
+
+  const addEmployee = function () {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "firstName",
+          message: "Employee's first name",
+        },
+        {
+          type: "input",
+          name: "lastName",
+          message: "Employee's last name",
+        },
+        {
+          type: "input",
+          name: "manager",
+          message: "Employee's manager",
+        },
+        {
+          type: "input",
+          name: "role",
+          message: "Employee's role",
+        },
+      ])
+      .then((answers) => {
+        let { firstName, lastName, manager, role } = answers;
+        if (!manager) manager = null;
+        connection.query(
+          "INSERT INTO `employee` (first_name, last_name, manager_id, role_id) VALUES (?, ?, ?, ?)",
+          [firstName, lastName, manager, role],
+          function (err, results) {
+            if (err) throw new Error(err);
+            console.log("Employee Added");
+            main();
+          }
+        );
+      });
+  };
